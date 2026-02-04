06764de2e18f9064d8f49a0bc4ad36a3a3e933da
Justine Jae <bongalos.justinejaeg8@gmail.com>
2026-02-05 06:59:58 +0800

    separated the UI and CLI version

--- CLI_Ver.py
+++ CLI_Ver.py
@@ -3,26 +3,3 @@ from openpyxl import Workbook, load_workbook
 grades = "Sample Grades DB.xlsx"
 wb1 = load_workbook(grades)
 ws1 = wb1.active
-
-class faculty:
-    
-
-
-def print_grades(student_id):
-    for row in ws1.iter_rows(min_row=2,values_only=True):
-        if row[1] == student_id:
-            return {"sub1":row[2],"sub2":row[3],"sub3":row[4],"sub4":row[5],"sub5":row[6],"sub6":row[7],"sub7":row[8],"sub8":row[9]}
-    return False
-
-def print_all_students():
-    Data = {}
-    for row in ws1.iter_rows(min_row=2,values_only=True):
-        if row[0] != None:
-            Data[row[0]] = [row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]]
-        else:
-            continue
-    return Data
-    
-
-
-
---
+++ UI_version/Front_End.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import messagebox
+
+
+mainwindow = Tk()
+
+mainwindow.mainloop()
